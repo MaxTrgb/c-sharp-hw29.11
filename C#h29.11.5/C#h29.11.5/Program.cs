52629f8558972492a783fadb195e98d4082bc2b1
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2023-11-26 16:38:13 +0200

    Add files via upload

---
+++ C#h29.11.5/C#h29.11.5/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MySpace
+{
+    interface IOutput
+    {
+        void Show();
+        void Show(string info);
+    }
+
+    class OutputExample : IOutput
+    {
+        // Implement the Show method
+        public void Show()
+        {
+            Console.WriteLine("Displaying information.");
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine($"Displaying information: {info}");
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            OutputExample outputExample = new OutputExample();
+
+            outputExample.Show();
+
+            outputExample.Show("Additional Message");
+        }
+    }
+}
---
+++ C#hw29.11.4/C#hw29.11.4/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace MySpace
+{
+    // Define the ISort interface
+    interface ISort
+    {
+        void SortAsc();
+        void SortDesc();
+    }
+
+    // Implement the ISort interface in a class
+    class SortingExample : ISort
+    {
+        private int[] numbers;
+
+        // Constructor
+        public SortingExample(int[] array)
+        {
+            numbers = array;
+        }
+
+        // Method to display array elements
+        public void DisplayArray()
+        {
+            Console.WriteLine("Array elements:");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // Implement the SortAsc method
+        public void SortAsc()
+        {
+            Array.Sort(numbers);
+            Console.WriteLine("Array sorted in ascending order.");
+        }
+
+        // Implement the SortDesc method
+        public void SortDesc()
+        {
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+            Console.WriteLine("Array sorted in descending order.");
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Example usage of the ISort interface
+            int[] arrayToSort = { 5, 2, 8, 1, 3 };
+
+            // Create an object of the SortingExample class
+            SortingExample sorter = new SortingExample(arrayToSort);
+
+            sorter.DisplayArray();
+
+            sorter.SortAsc();
+            sorter.DisplayArray();
+
+            sorter.SortDesc();
+            sorter.DisplayArray();
+        }
+    }
+}
